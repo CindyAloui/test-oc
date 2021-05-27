4df0b5dac16d99f711b7a27a788bec2f8b26f7b6
Cindy.a <cindy.aloui@gmail.com>
2021-05-27 20:50:02 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import flask
+#from flask_cors import CORS
+import json
+import os
+import tensorflow
+
+# Initializing flask app
+app = flask.Flask(__name__)
+
+# Adding cors to flask
+#CORS(app)
+
+# Controller-1
+@app.route("/", methods=['GET'])
+def hello_world():
+    return 'Hello, !'
+
+
+# Running the api
+if __name__ == '__main__':
+     app.run(debug=True)
